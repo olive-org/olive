c0da8ad49475806e2905ce922e8a008aa611f851
lack <598223084@qq.com>
2023-06-27 18:59:42 +0800

    fatm: 更新 examples

--- examples/schema.rs
+++ examples/schema.rs
@@ -3,14 +3,16 @@
 use bpmn_schema::*;
 
 fn main() {
+    let mut d = Definitions {
+        ..Default::default()
+    };
+
     let mut process = Process {
         id: Some("process1".to_string()),
-        flow_elements: vec![
-            FlowElement::StartEvent(StartEvent {
-                id: Some("start".to_string()),
-                ..Default::default()
-            }),
-        ],
+        flow_elements: vec![FlowElement::StartEvent(StartEvent {
+            id: Some("start".to_string()),
+            ..Default::default()
+        })],
         ..Default::default()
     };
 
@@ -41,4 +43,10 @@ fn main() {
     assert_eq!(seq_flow.id(), &Some("test".to_string()));
     assert_eq!(seq_flow.source_ref(), "start");
     assert_eq!(seq_flow.target_ref(), "end");
+
+    d.set_id(Some("1".to_string()));
+    d.root_elements_mut().push(RootElement::Process(process));
+
+    let out = serde_json::to_string(&d).unwrap();
+    println!("{:?}", out);
 }
